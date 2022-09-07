eab9202e0b9e8d9ed060918b6e6208c7f51efe1f
darosio <darosio@users.noreply.github.com>
2022-09-07 16:55:33 +0200

    fix python deps >3.8; <3.11 (#6)
    * fix: python deps in pyproject and update poetry 1.2.0
    
    * bump: version 0.3.3 → 0.3.4
    
    * bump: workflow tests
    
    * ci: tests workflow with constraints
    
    * ci: including pandoc install in Tests workflow
    
    * ci: fixing
    
    * ci: fixing 02
    
    * ci: fixing 03
    
    * ci: without coverage but with release

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Daniele Arosio'
 author = 'Daniele Arosio'
 
 # The full version, including alpha/beta/rc tags
-release = "0.3.3"
+release = "0.3.4"
 
 # -- General configuration ---------------------------------------------------
 
