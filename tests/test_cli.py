de9e2272558508403580489776e8a669917f606a
daniele arosio <daniele.arosio@cnr.it>
2022-09-22 17:42:04 +0200

    fix: test removing os.chdir()

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,8 @@
 """Test ``clop`` cli."""
 from __future__ import annotations
 
+from typing import Any
+
 from click.testing import CliRunner
 
 from clophfit.__main__ import clop
@@ -14,7 +16,7 @@ def test_eq1() -> None:
     assert "4." in result.output
 
 
-def test_prtecan(tmp_path) -> None:
+def test_prtecan(tmp_path: Any) -> None:
     """It runs XXX pr.tecan and generates correct results and graphs."""
     out = tmp_path / "out3"
     out.mkdir()
