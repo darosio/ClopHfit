651693331c64c98d969dba7977cf1a5c16add850
daniele arosio <daniele.arosio@cnr.it>
2022-10-21 10:18:36 +0200

    test: win

--- tests/test_oldscripts.py
+++ tests/test_oldscripts.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 import subprocess
 import typing
 from pathlib import Path
-from pathlib import PurePath
 from typing import Any
 from typing import Iterator
 from typing import List
