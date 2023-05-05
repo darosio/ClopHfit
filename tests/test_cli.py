0cc7a809e968ad8c64032fa931f4c3efd82c11ab
daniele arosio <daniele.arosio@cnr.it>
2023-05-05 14:17:19 +0200

    fix: test_cli (os.path -> Path)

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,10 +1,13 @@
 """Test ``clop`` cli."""
 from __future__ import annotations
 
+import filecmp
 from pathlib import Path
 
 import pytest
 from click.testing import CliRunner
+from matplotlib.testing.compare import compare_images  # type: ignore
+from matplotlib.testing.exceptions import ImageComparisonFailure  # type: ignore
 
 from clophfit.__main__ import clop
 
@@ -15,6 +18,32 @@ def test_eq1() -> None:
     result = runner.invoke(clop, ["eq1", "2", "2", "2"])
     assert result.exit_code == 0
     assert "4." in result.output
+
+
+def test_prenspire(tmp_path: Path) -> None:
+    """Run cli with actual data."""
+    expected = Path(__file__).parent / "EnSpire" / "data" / "output"
+    out = tmp_path / "E"
+    out.mkdir()
+    runner = CliRunner()
+    result = runner.invoke(
+        clop,
+        ["prenspire", "tests/EnSpire/data/NTT_37C_pKa.csv", "--out", str(out)],
+    )
+    assert result.exit_code == 0
+    # validate output files
+    assert (out / "NTT_37C_pKa_A.csv").exists()
+    assert (out / "NTT_37C_pKa_B.csv").exists()
+    assert (out / "NTT_37C_pKa_A.png").exists()
+    assert (out / "NTT_37C_pKa_B.png").exists()
+    # validate output file contents
+    assert filecmp.cmp(out / "NTT_37C_pKa_A.csv", expected / "NTT_37C_pKa_A.csv")
+    assert filecmp.cmp(out / "NTT_37C_pKa_B.csv", expected / "NTT_37C_pKa_B.csv")
+    # validate graph
+    for f in ["NTT_37C_pKa_A.png", "NTT_37C_pKa_B.png"]:
+        msg = compare_images(out / f, expected / f, 0.0001)
+        if msg:
+            raise ImageComparisonFailure(msg)
 
 
 @pytest.mark.filterwarnings("ignore:OVER")
