6820744f9c1a270779336bfd4ea79d1bc2258335
daniele arosio <daniele.arosio@cnr.it>
2024-07-01 18:17:13 +0200

    refactor: Drop pkg_resources in favor of importlib.metadata

--- src/clophfit/__init__.py
+++ src/clophfit/__init__.py
@@ -1,7 +1,11 @@
 """ClopHfit: Parse plate-reader and fit ClopHensor titrations."""
 
-from pkg_resources import get_distribution
+from importlib.metadata import PackageNotFoundError, version
 
-__version__ = get_distribution("clophfit").version
+try:
+    __version__ = version("clophfit")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
 __enspire_out_dir__ = f"Meas-{__version__}"
 __tecan_out_dir__ = f"out-{__version__}"
